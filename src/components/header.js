f170586db29a92d60fb5ad3adaab93c841aa98e5
Randy Camacho <43118312+RandyCamacho@users.noreply.github.com>
2021-01-29 22:59:26 -0700

    backup

---
+++ src/components/header.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { Link } from "gatsby";
+
+import { HeaderNav, Logo, Menu } from "../styles/headerStyles";
+import { Container, FlexBox } from "../styles/globalStyles";
+
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext";
+
+const Header = ({ onCursor, toggleMenu, setToggleMenu }) => {
+  const dispatch = useGlobalDispatchContext();
+  const { currentTheme } = useGlobalStateContext();
+
+  const toggleTheme = () => {
+    if (currentTheme === "dark") {
+      dispatch({ type: "TOGGLE_THEME", theme: "light" });
+    } else {
+      dispatch({ type: "TOGGLE_THEME", theme: "dark" });
+    }
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", currentTheme);
+  }, [currentTheme]);
+
+  return (
+    <HeaderNav
+      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: -72, opacity: 1 }}
+      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
+    >
+      <Container>
+        {console.log({ currentTheme })}
+        <FlexBox spaceBetween noHeight>
+          <Logo
+            onMouseEnter={() => onCursor("hovered")}
+            onMouseLeave={onCursor}
+          >
+            <Link to="/">FURR</Link>
+            <span
+              onClick={toggleTheme}
+              onMouseEnter={() => onCursor("pointer")}
+              onMouseLeave={onCursor}
+            ></span>
+            <Link to="/">W</Link>
+          </Logo>
+          <Menu
+            onClick={() => {
+              setToggleMenu(!toggleMenu);
+            }}
+          >
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </FlexBox>
+      </Container>
+    </HeaderNav>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Layout from "../components/layout";
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext";
+import HomeBanner from "../components/homepage/HomeBanner";
+import HomeContent from "../components/homepage/HomeContent";
+import HomeFeature from "../components/homepage/HomeFeature";
+import HomeAbout from "../components/homepage/HomeAbout";
+
+const IndexPage = (props) => {
+  const { currentTheme, cursorStyle } = useGlobalStateContext();
+  const dispatch = useGlobalDispatchContext();
+
+  const onCursor = (cursorType) => {
+    cursorType = (cursorStyle.includes(cursorType) && cursorType) || false;
+    dispatch({ type: "CURSOR_TYPE", cursorType: cursorType });
+  };
+
+  return (
+    <Layout>
+      <HomeBanner onCursor={onCursor} />
+      <HomeContent />
+      <HomeFeature onCursor={onCursor} />
+      <HomeAbout onCursor={onCursor} />
+    </Layout>
+  );
+};
+
+export default IndexPage;
